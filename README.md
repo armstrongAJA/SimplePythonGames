0460bbeb5ad4b2cbf9c730838f8729578a6c4625
armstrongAJA <alex.armstrong192@gmail.com>
2025-02-28 16:51:43 +0000

    Mechanics all programmed, just need to deal with bugs
    Completed win checks, zero propagation, resetting board, changing game settings. Board size changes are causing issues with cell coordinates above 10 (only issue when board size is above 10).

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SimplePythonGames
-This project is a few simple python based games developed to get used to TKinter and pygame libraries. Each files is its own self contained game. 
+This project is a few simple python based games developed to get used to TKinter and pygame libraries. Each file is its own self contained game. 
--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -12,36 +12,46 @@ class Grid():
         self.cellContents = {}
         self.mineImage = PhotoImage(file = r"mine.png").subsample(13,13)
         self.flagImage = PhotoImage(file = r"flag.png").subsample(30,30)
-        
+        self.unclearedBoxes = []
+        self.taggedBoxes = []
+       
     def createGrid(self, window):
         buttons = {}
         for x in range(self.gridSize):
             for y in range(self.gridSize):
-                print('adding button:',x,y)
                 buttons["btn{}{}".format(x,y)] = Button(window, text = "" , fg='black', state='normal', width = self.buttonSize)
                 current_button=buttons["btn{}{}".format(x,y)]
                 current_button.bind('<Button-1>', lambda btn=current_button,xx=x,yy=y: self.leftClick(xx,yy,btn))
                 current_button.bind('<Button-2>', lambda btn=current_button,xx=x,yy=y: self.rightClick(xx,yy,btn))
                 # set Button grid
                 current_button.grid(column=x, row=y)
+        self.buttons = buttons
+                
         resetButton = Button(window, text = "Reset" , fg='black', state='normal', height = self.buttonSize, command = self.resetBoard)
         resetButton.grid(column=0, row = self.gridSize+2, columnspan = self.gridSize)
-        self.buttons = buttons
+
+        settingsButton = Button(window, text = "Settings" , fg='black', state='normal', height = self.buttonSize, command = settingsWindow.openSettings)
+        settingsButton.grid(column=0, row = self.gridSize+3, columnspan = self.gridSize)
         
     def leftClick(self,xx,yy,btn):
         print('left clicked button ',xx,' ',yy)
         if self.cellContents[(xx,yy)]=='m':
-            self.buttons["btn{}{}".format(xx,yy)]['text'] = ''
-            self.buttons["btn{}{}".format(xx,yy)].config(image = self.mineImage)
-            gameOver()
+            self.revealBox(xx,yy)
         else:
-            self.buttons["btn{}{}".format(xx,yy)]['text'] = self.cellContents[(xx,yy)]
-            self.revealSurroundings(xx,yy)
+            self.revealBox(xx,yy)
+            x,y, revealed = self.revealSurroundings(xx,yy)
+            self.revealBlanks(xx,yy,revealed)
+        winCheck(self.numberOfMines, self.gridSize, self.unclearedBoxes)
         
         
     def rightClick(self,xx,yy,btn):
         print('right clicked button ',xx,' ',yy)
-        self.buttons["btn{}{}".format(xx,yy)].config(image = self.flagImage)
+        if [xx,yy] not in self.taggedBoxes:
+            self.buttons["btn{}{}".format(xx,yy)].config(image = self.flagImage)
+            self.taggedBoxes.append([xx,yy])
+        else:
+            self.buttons["btn{}{}".format(xx,yy)].config(image = '')
+            self.taggedBoxes.remove([xx,yy])
 
     def generateMinePositions(self):
         mineCoords = []
@@ -61,6 +71,8 @@ class Grid():
         self.mineCoords = mineCoords
 
     def resetBoard(self):
+        self.unclearedBoxes = []
+        self.taggedBoxes = []
         message = startMessage
         updateMessage(message)
         self.generateMinePositions()
@@ -94,25 +106,180 @@ class Grid():
                 else:
                     self.buttons["btn{}{}".format(x,y)].config(text = self.cellContents[(x,y)])
 
+    def revealBox(self,x,y):
+        if self.cellContents[(x,y)] == 'm':
+            self.buttons["btn{}{}".format(x,y)].config(image = self.mineImage)
+            self.buttons["btn{}{}".format(x,y)].config(text = '')
+            gameOver()
+        else:
+            self.buttons["btn{}{}".format(x,y)].config(text = self.cellContents[(x,y)])
+            if [x,y] not in self.unclearedBoxes:
+                self.unclearedBoxes.append([x,y])
+            
     def revealSurroundings(self, x,y):
-        for xx in [-1,0,1]:
-            for yy in [-1,0,1]:
-                if xx==0 and yy==0:
-                    pass
-                elif self.cellContents[(x+xx,y+yy)] != 'm':
-                    self.buttons["btn{}{}".format(x+xx,y+yy)].config(text = self.cellContents[(x+xx,y+yy)])
-                    self.buttons["btn{}{}".format(x+xx,y+yy)].config(image = '')
+        revealed = []
+        if self.cellContents[(x,y)] == 0:
+            for xx in [-1,0,1]:
+                for yy in [-1,0,1]:
+                    try:
+                        if xx==0 and yy==0:
+                            pass
+                        elif self.cellContents[(x+xx,y+yy)] != 'm':
+                            self.revealBox(x+xx,y+yy)
+
+                            revealed.append([x+xx,y+yy])
+                    except:
+                        print('edge cell clicked...')
+        return x,y,revealed
+
+    def revealBlanks(self,x,y, revealed):
+        #add revealed cells to a checklist
+        checkList=revealed
+        checkedCells = []
+        while len(checkList)>0:
+            #check all cells in checklist for zero in surroundings
+            for cell in checkList:
+                x = cell[0]
+                y = cell[1]
+                for xx in [-1,0,1]:
+                    for yy in [-1,0,1]:
+                        try:
+                            if self.cellContents[(x+xx,y+yy)] == 0:
+                            #reveal any zeroes
+                                self.revealBox(x+xx,y+yy)
+                                self.revealSurroundings(x+xx,y+yy)
+                            #add any zero cell coordinates to checkList
+                                if [x+xx,y+yy] not in checkList and [x+xx,y+yy] not in checkedCells:
+                                    checkList.append([x+xx,y+yy])
+                        except:
+                            print('cell ({},{}) not detected'.format(x+xx,y+yy))
+                            pass
+                        checkedCells.append([x+xx,y+yy])
+                checkList.remove(cell)
+                print('length of checklist:',len(checkList), '\nchecklist:',checkList)
+
+    def refreshGrid(self, gridsize, numberofmines):
+        global lbl
+        global startMessage
+        
+        buttons = {}
+        self.gridSize = gridsize
+        self.numberOfMines = numberofmines
+
+        widgets = root.grid_slaves()
+        for l in widgets:
+            l.destroy()
+            
+        message = startMessage
+        lbl = Label(root, text = message)
+        lbl.grid(column=0, row = self.gridSize+1, columnspan = self.gridSize)
+        for x in range(self.gridSize):
+            for y in range(self.gridSize):
+                buttons["btn{}{}".format(x,y)] = Button(root, text = "" , fg='black', state='normal', width = self.buttonSize)
+                current_button=buttons["btn{}{}".format(x,y)]
+                current_button.bind('<Button-1>', lambda btn=current_button,xx=x,yy=y: self.leftClick(xx,yy,btn))
+                current_button.bind('<Button-2>', lambda btn=current_button,xx=x,yy=y: self.rightClick(xx,yy,btn))
+                # set Button grid
+                current_button.grid(column=x, row=y)
+        self.buttons = buttons
+                
+        resetButton = Button(root, text = "Reset" , fg='black', state='normal', height = self.buttonSize, command = self.resetBoard)
+        resetButton.grid(column=0, row = self.gridSize+2, columnspan = self.gridSize)
+
+        settingsButton = Button(root, text = "Settings" , fg='black', state='normal', height = self.buttonSize, command = settingsWindow.openSettings)
+        settingsButton.grid(column=0, row = self.gridSize+3, columnspan = self.gridSize)
+        self.resetBoard()
+        
+                            
+            
 
 def gameOver():
         message = 'You Lose'
         updateMessage(message)
+
+def onWin():
+    message = 'You Win!'
+    updateMessage(message)
+
+def winCheck(numberOfMines,gridSize,clearedBoxes):
+    if numberOfMines==gridSize**2-len(clearedBoxes):
+        onWin()
+    print('unclearedBoxes:',gridSize**2-len(clearedBoxes))
         
 def updateMessage(message):
     lbl.config(text = message)
+
+class SettingsWindow():
+    def __init__(self, numberOfMines, gridSize):
+        self.numberOfMines = numberOfMines
+        self.gridSize = gridSize
+        self.buttonSize = 25
+    
+    def openSettings(self):
+        print('settings window opening...')
+        self.settings = Tk()
+        self.settings.geometry('1000x700')
+        
+        self.minesLabel = Label(self.settings, text = 'Number of mines: {}'.format(self.numberOfMines))
+        self.minesLabel.grid(column=0, row=2)
+        
+        self.gridSizeLabel = Label(self.settings, text = 'Grid size: {}'.format(self.gridSize))
+        self.gridSizeLabel.grid(column=0, row=3)
+        
+        increaseMinesButton = Button(self.settings, text = "increase mines" , fg='black', state='normal', height = self.buttonSize, command = self.increaseMines)
+        increaseMinesButton.grid(column=0, row =1)
+
+        decreaseMinesButton = Button(self.settings, text = "decrease mines" , fg='black', state='normal', height = self.buttonSize, command = self.decreaseMines)
+        decreaseMinesButton.grid(column=1, row =1)
+
+        increaseGridSizeButton = Button(self.settings, text = "increase grid size" , fg='black', state='normal', height = self.buttonSize, command = self.increaseGridSize)
+        increaseGridSizeButton.grid(column=2, row =1)
+
+        decreaseGridSizeButton = Button(self.settings, text = "decrease grid size" , fg='black', state='normal', height = self.buttonSize, command = self.decreaseGridSize)
+        decreaseGridSizeButton.grid(column=3, row =1)
+
+        okButton = Button(self.settings, text = "OK" , fg='black', state='normal', height = self.buttonSize, command = self.applySettings)
+        okButton.grid(column=1, row =3)
+        
+
+    def increaseMines(self):
+        self.numberOfMines+=1
+        print('number of mines increased...')
+        self.updateMessages()
+
+    def decreaseMines(self):
+        self.numberOfMines-=1
+        print('number of mines decreased...')
+        self.updateMessages()
+
+    def increaseGridSize(self):
+        self.gridSize+=1
+        print('grid size increased...')
+        self.updateMessages()
+
+    def decreaseGridSize(self):
+        self.gridSize-=1
+        print('grid size decreased...')
+        self.updateMessages()
+
+    def applySettings(self):
+        print('applying settings...')
+        grid.refreshGrid(self.gridSize, self.numberOfMines)
+        self.settings.destroy()
+
+
+    def updateMessages(self):
+        self.minesLabel['text'] = 'Number of mines: {}'.format(self.numberOfMines)
+        self.gridSizeLabel['text'] = 'Grid size: {}'.format(self.gridSize)
+        
         
 def main():
     global startMessage
     global lbl
+    global grid
+    global settingsWindow
+    global root
+    
     # create root window
     root = Tk()
     root.geometry('1000x700')
@@ -127,6 +294,8 @@ def main():
     # adding a label to the root window
 
     grid = Grid(gridSize, numberOfMines)
+    settingsWindow = SettingsWindow(numberOfMines, gridSize)
+    
     grid.createGrid(root)
     startMessage = "Click to remove cell, right click to flag cell!"
     message = startMessage
